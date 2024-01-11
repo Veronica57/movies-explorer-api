445b17614f5babbfe8169eec7feed41fd92984c7
Veronika Pinchuk <tveronichka@gmail.com>
2024-01-11 23:34:19 +0300

    fixes

--- middlewares/error.js
+++ middlewares/error.js
@@ -1,10 +1,19 @@
-const INTERNAL_SERVER_ERROR_CODE = require('../utils/codes');
+const INTERNAL_SERVER_ERROR_CODE = require("../utils/codes");
 
 module.exports = (err, req, res, next) => {
-  if (err.statusCode) {
-    res.status(err.statusCode).send({ message: err.message });
-  } else {
-    res.status(INTERNAL_SERVER_ERROR_CODE).send({ message: 'Internal Server Error' });
-  }
-  next();
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.header("Access-Control-Allow-Credentials", true);
+    if (err.statusCode) {
+        res.status(err.statusCode).send({ message: err.message });
+    } else {
+        res.status(INTERNAL_SERVER_ERROR_CODE).send({
+            message: "Internal Server Error",
+        });
+    }
+    next();
 };

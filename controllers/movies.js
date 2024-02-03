6902100ddbf6a88e019d48d0db79426aa7de1316
Veronika Pinchuk <tveronichka@gmail.com>
2024-02-03 03:06:07 +0300

    movie controller fix

--- controllers/movies.js
+++ controllers/movies.js
@@ -1,63 +1,64 @@
-const { CastError, ValidationError } = require('mongoose').Error;
 const NotFoundError = require('../errors/notfound');
-const BadRequestError = require('../errors/badrequest');
 const ForbiddenError = require('../errors/forbidden');
 
 // movie import from schema
 const Movie = require('../models/movie');
 
 // create movie
-const createMovie = async (req, res, next) => {
-    const owner = req.user._id;
-    try {
-        const movie = await Movie.create({ ...req.body, owner });
-        const populatedMovie = await movie.populate('owner');
-        res.status(201).send(populatedMovie);
-    } catch (error) {
-        if (error instanceof ValidationError) {
-            const errorMessage = Object.values(error.errors)
-                .map((err) => err.message)
-                .join(', ');
-            next(new BadRequestError(`Invalid data ${errorMessage}`));
-        } else {
-            next(error);
-        }
-    }
+const createMovie = (req, res, next) => {
+  const owner = req.user._id;
+  const {
+    country,
+    director,
+    duration,
+    year,
+    description,
+    image,
+    trailerLink,
+    nameRU,
+    nameEN,
+    thumbnail,
+    movieId
+  } = req.body;
+
+  Movie.create({
+    country,
+    director,
+    duration,
+    year,
+    description,
+    image,
+    trailerLink,
+    nameRU,
+    nameEN,
+    thumbnail,
+    movieId,
+    owner
+  })
+    .then((movie) => res.status(201).send(movie))
+    .catch(next);
 };
   
 // get movies
-const getMovies = async (req, res, next) => {
-    try {
-        const userId = req.user._id;
-        const movies = await Movie.find({ owner: userId }).populate['owner'];
-        res.send(movies);
-    } catch (error) {
-        next(error);
-    }
+const getMovies = (req, res, next) => {
+  Movie.find({ owner: req.user._id })
+    .orFail(() => new NotFoundError('Movie Not Found'))
+    .then((movies) => res.status(200).send(movies))
+    .catch(next);
 };
 
 // delete movie by ID
-const deleteMovie = async (req, res, next) => {
-    const movieId = req.params.id;
-    const userId = req.user._id;
-
-    try {
-        const movie = await Movie.findById(movieId);
-        if (!movie) {
-            throw new NotFoundError('Movie Not Found');
-        }
-        if (userId !== movie.owner.toString()) {
-            throw new ForbiddenError('Impossible to delete');
-        }
-        await Movie.deleteOne({ _id: movieId });
-        res.send({ message: 'Movie was successfully deleted' });
-    } catch (error) {
-        if (error instanceof CastError) {
-            next(new BadRequestError('Invalid movie ID'));
-        } else {
-            next(error);
-        }
-    }
+const deleteMovie = (req, res, next) => {
+  Movie.findById(req.params.id)
+    .then((movie) => {
+      if (!movie) throw new NotFoundError('Movie Not Found');
+      else if (req.user._id !== movie.owner.toString()) {
+        throw new ForbiddenError('Impossible to delete');
+      } else {
+        return movie.deleteOne().then(() => res.send(movie));
+      }
+    })
+    .catch(next);
 };
 
 module.exports = {
